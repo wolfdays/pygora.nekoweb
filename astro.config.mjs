3c3879118fa5f17d30f10c962b5b8ef83ac973dc
wolfdays <wolfdaymax@gmail.com>
2025-06-27 22:58:32 +0200

    astro setup + deploy fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import nekoweb from '@indiefellas/astro-adapter-nekoweb';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://www.pygora.nekoweb.org',
+    outDir: 'dist/',
+    adapter: nekoweb({       
+        apiKey: '2441c8c5f1c4bfa4b7847604d203bdba836a01d0d4bf139a698fb09ba12786cf',
+        cookie: '6719d393268d41ff2cbfd8c8bb3ea278d279fb1847fd03237b59219afca32a99',
+        folder: 'dist'
+    })
+});
